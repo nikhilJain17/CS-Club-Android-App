43e258f434e98202b0483235c6e5591e06b8241b
nikhilJain17 <nikhil.jain0987@gmail.com>
2015-10-27 20:55:01 -0400

    Created LoginActivity during meeting

---
+++ Up2Date2/app/src/main/java/com/csclub/up2date/HighlightsActivity.java
@@ -0,0 +1,13 @@
+package com.csclub.up2date;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HighlightsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_highlights);
+    }
+}
--- Up2Date2/app/src/main/java/com/csclub/up2date/LoginActivity.java
+++ Up2Date2/app/src/main/java/com/csclub/up2date/LoginActivity.java
@@ -1,13 +1,66 @@
 package com.csclub.up2date;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    // 1. get username, store it in string
+    // 2. get password, store it in string
+    // 3. send to server
+    // 4. add button listeners
+    // 5.
+
+    String username;
+    String password;
+
+
+    EditText usernameET;
+    EditText passwordET;
+
+    Button guestLogin, accountLogin, signup, forgotPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        usernameET = (EditText) findViewById(R.id.usernameET);
+        passwordET = (EditText) findViewById(R.id.passwordET);
+
+        accountLogin = (Button) findViewById(R.id.loginButton);
+        signup = (Button) findViewById(R.id.signupButton);
+
     }
+
+    // Signup Button Listener
+    public void onSignupButtonClicked(View v) {
+
+        // Take us to another activity
+        Intent myIntent = new Intent(getApplicationContext(), SignupActivity.class);
+        startActivity(myIntent);
+
+    }
+
+
+    // Button Listener for account button
+    public void onAccountButtonClicked(View v) {
+
+        // get text from the edittext
+        // store it in a string
+        username = usernameET.getText().toString();
+        password = passwordET.getText().toString();
+
+        Toast.makeText(getApplicationContext(), "Welcome " + username, Toast.LENGTH_SHORT).show();
+
+    }
+
+
+
 }
